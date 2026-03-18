b06916a865cd5e64f8477fe94e0c1a37b6224f62
Fey Xie <feyxiexzf@gmail.com>
2026-03-18 14:58:48 +0800

    update deps

--- plugin/build.gradle.kts
+++ plugin/build.gradle.kts
@@ -1,13 +1,13 @@
 plugins {
-    kotlin("jvm") version "2.3.10"
-    kotlin("kapt") version "2.3.10"
+    kotlin("jvm") version "2.3.20"
+    kotlin("kapt") version "2.3.20"
 }
 
 group = "dev.ujhhgtg"
 version = "unspecified"
 
 dependencies {
-    compileOnly("org.jetbrains.kotlin:kotlin-compiler-embeddable:2.3.10")
+    compileOnly("org.jetbrains.kotlin:kotlin-compiler-embeddable:2.3.20")
     compileOnly("com.google.auto.service:auto-service-annotations:1.1.1")
     kapt("com.google.auto.service:auto-service:1.1.1")
 }
