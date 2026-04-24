48fde4dc886abd91d83a6597c25c19223510210f
Fey Xie <feyxiexzf@gmail.com>
2026-04-24 22:30:59 +0800

    bump kotlin

--- plugin/build.gradle.kts
+++ plugin/build.gradle.kts
@@ -1,13 +1,13 @@
 plugins {
-    kotlin("jvm") version "2.3.20"
-    kotlin("kapt") version "2.3.20"
+    kotlin("jvm") version "2.3.21"
+    kotlin("kapt") version "2.3.21"
 }
 
 group = "dev.ujhhgtg"
 version = "unspecified"
 
 dependencies {
-    compileOnly("org.jetbrains.kotlin:kotlin-compiler-embeddable:2.3.20")
+    compileOnly("org.jetbrains.kotlin:kotlin-compiler-embeddable:2.3.21")
     compileOnly("com.google.auto.service:auto-service-annotations:1.1.1")
     kapt("com.google.auto.service:auto-service:1.1.1")
 }
